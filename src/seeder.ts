dd2f0d8d3a3d419e8e444f38d96381833113e972
Thiago Pacheco <thiagodelimapacheco@gmail.com>
2021-06-04 19:11:20 -0400

    Created seed class

--- src/seeder.ts
+++ src/seeder.ts
@@ -1,10 +1,23 @@
 // @ts-ignore
-import seeder from "mongoose-seed";
+import seeder from 'mongoose-seed';
 
-const BASE_PATH = "lib/src/";
+const BASE_PATH = 'dist/src/';
 
 const formatPath = (path: any) => `${BASE_PATH}${path}`;
 
+export class Seed {
+  model!: string;
+  path!: string;
+  clear: boolean = false;
+  documents!: any[];
+}
+
+/**
+ * Function to seed a Mongo database with initial data
+ * @param uri : Mongo DB URI
+ * @param seeds : List of seed objects
+ * @returns null
+ */
 export const Seeder = (uri: any, seeds = []) => {
   return new Promise((resolve) => {
     seeder.connect(uri, function () {
