0deb63f8f0c2d6a47874fbceee9b34be23443599
Thiago Pacheco <thiagodelimapacheco@gmail.com>
2021-06-03 20:35:01 -0400

    included npm workflow into push

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 export * as auth from './auth';
-export * as database from './database';
+export { Connection } from './database';
 export * as exceptions from './exceptions';
 export * as helpers from './helpers';
 export * as middlewares from './middlewares';
-export * as repositories from './repositories';
+export * as BaseRepository from './repositories';
 export * as seeder from './seeder';
 export { Server } from './server';
